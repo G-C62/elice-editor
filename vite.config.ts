254511e9c86bde94254e653f0f7d02ea76178b31
G-C62 <nsw9211@gmail.com>
2025-06-15 23:06:47 +0900

    vite-plugin-monaco-editor-esm 패키지 추가 및 Vite 설정에 MonacoEditorPlugin 통합

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import MonacoEditorPlugin from 'vite-plugin-monaco-editor-esm';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,6 +11,7 @@ export default defineConfig({
         plugins: ['@emotion/babel-plugin'],
       },
     }),
+    MonacoEditorPlugin({}),
     tsconfigPaths(),
   ],
 });
